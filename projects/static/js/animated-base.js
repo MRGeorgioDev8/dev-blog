cd231e04f08988f94a092eb37cf6bf476b0d5cff
Georgio <georgetrueman64@gmail.com>
2024-05-30 14:44:58 +0300

    update

--- projects/static/js/animated-base.js
+++ projects/static/js/animated-base.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const devContainer = document.querySelector('.column.card');
 
     gsap.set(devContainer, { x: -100, opacity: 0 });
-    gsap.to(devContainer, { x: 40, opacity: 1, duration: 1, delay: 0.3, ease: "bounce.inOut" });
+    gsap.to(devContainer, { x: 40, opacity: 1, duration: 1, delay: 0.5, ease: "bounce.out" });
 
     gsap.set(logo, { scale: 2 });
     gsap.to(logo, { scale: 1, duration: 1, delay: 0.3, ease: "power1.inOut" });
