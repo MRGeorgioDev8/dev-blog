73dc7d288937df15dd7c40fcc54bf14ab870a8f3
Georgio <georgetrueman64@gmail.com>
2024-06-04 22:02:29 +0300

    update

--- projects/static/js/animated-base.js
+++ projects/static/js/animated-base.js
@@ -8,9 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const messagesList = document.querySelector('.messages');
     const inboxTitle = document.querySelector('.inbox__title');
     const messageContainer = document.querySelector('.message')
+    const cardContainer = document.querySelector('.auth')
 
     gsap.set(devContainer, { x: -100, opacity: 0 });
-    gsap.to(devContainer, { x: 40, opacity: 1, duration: 1, delay: 0.5, ease: "bounce.out" });
+    gsap.to(devContainer, { x: 40, opacity: 1, duration: 1, delay: 0.3, ease: "bounce.out" });
 
     gsap.set(logo, { scale: 2 });
     gsap.to(logo, { scale: 1, duration: 1, delay: 0.3, ease: "power1.inOut" });
@@ -38,4 +39,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     gsap.set(messageContainer, {scale:0, y: -100, opacity: 0 });
     gsap.to(messageContainer, {scale: 1, y: 0, opacity: 1, duration: 0.6, ease: "quad.inOut", delay: 0.3 });
+
+    gsap.set(cardContainer, {opacity: 0 });
+    gsap.to(cardContainer, {opacity: 1, ease:"quad.inOut", duration: 0.7 });
 });
